9e117cd94ac6300bc9beeb6edc36f4724acb0097
AdrianoGullo <adrianogullo1@gmail.com>
2023-12-22 13:07:06 -0300

    Imagens dos planetas

--- Gravitational Slingshot/gravity.py
+++ Gravitational Slingshot/gravity.py
@@ -6,7 +6,9 @@ width, height = 800, 600
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Gravitational Slingshot")
 FPS = 60
-tam_nave = 2
+tam_nave = 2        #m/s
+velocidade = 100    #m/s
+pesoNave = 10     #kg
 
 #Cores
 branco = (255, 255, 255)
@@ -32,20 +34,22 @@ class Planeta:
         self.gravidade = gravidade
         self.imagem = imagem
 
-# def criar_planetas():
-#     dados_planetas = [
-#         {"nome": "Mercúrio", "raio": 2439.7, "massa": 3.3011e23, "gravidade": 3.7, "imagem": "images/mercurio.jpg"},
-#         {"nome": "Vênus", "raio": 6051.8, "massa": 4.8675e24, "gravidade": 8.87, "imagem": "images/venus.jpg"},
-#         {"nome": "Terra", "raio": 6371, "massa": 5.972e24, "gravidade": 9.8, "imagem": "images/terra.jpg"},
-#         {"nome": "Marte", "raio": 3389.5, "massa": 6.4171e23, "gravidade": 3.7, "imagem": "images/marte.jpg"},
-#         {"nome": "Júpiter", "raio": 69911, "massa": 1.898e27, "gravidade": 24.79, "imagem": "images/jupiter.jpg"},
-#         {"nome": "Saturno", "raio": 58232, "massa": 5.683e26, "gravidade": 10.44, "imagem": "images/saturno.jpg"},
-#         {"nome": "Urano", "raio": 25362, "massa": 8.681e25, "gravidade": 8.69, "imagem": "images/urano.jpg"},
-#         {"nome": "Netuno", "raio": 24622, "massa": 1.0247e26, "gravidade": 11.15, "imagem": "images/netuno.jpg"}
-#     ]
-#
-#     tabela_dados = [Planeta(**dados) for dados in dados_planetas]
-#     return tabela_dados
+
+def criar_planetas():
+     dados_planetas = [
+         {"nome": "Mercúrio", "raio": 2439.7, "massa": 3.3011e23, "gravidade": 3.7, "imagem": "images/mercurio.png"},
+         {"nome": "Vênus", "raio": 6051.8, "massa": 4.8675e24, "gravidade": 8.87, "imagem": "images/venus.png"},
+         {"nome": "Terra", "raio": 6371, "massa": 5.972e24, "gravidade": 9.8, "imagem": "images/terra.png"},
+         {"nome": "Marte", "raio": 3389.5, "massa": 6.4171e23, "gravidade": 3.7, "imagem": "images/marte.png"},
+         {"nome": "Júpiter", "raio": 69911, "massa": 1.898e27, "gravidade": 24.79, "imagem": "images/jupiter.png"},
+         {"nome": "Saturno", "raio": 58232, "massa": 5.683e26, "gravidade": 10.44, "imagem": "images/saturno.png"},
+         {"nome": "Urano", "raio": 25362, "massa": 8.681e25, "gravidade": 8.69, "imagem": "images/urano.png"},
+         {"nome": "Netuno", "raio": 24622, "massa": 1.0247e26, "gravidade": 11.15, "imagem": "images/netuno.png"}
+     ]
+
+     tabela_dados = [Planeta(**dados) for dados in dados_planetas]
+     return tabela_dados
+
 
 def exibir_informacoes(planetas):
     for planeta in planetas:
@@ -53,10 +57,9 @@ def exibir_informacoes(planetas):
             f"{planeta.nome}: Raio = {planeta.raio} km, Massa = {planeta.massa} kg, "
             f"Gravidade = {planeta.gravidade} m/s²")
 
-#planetas = criar_planetas()
-velocidade = 100    # m/s
 
-background = pygame.transform.scale(pygame.image.load("images/background.gif"), (width, height))
+planetas = criar_planetas()
+background = pygame.transform.scale(pygame.image.load("images/background.png"), (width, height))
 #img_planeta = pygame.transform.scale(pygame.image.load(planetas[escolha].imagem), ((planetas[escolha].raio/1000)*2, (
 #        planetas[escolha].raio/1000)*2))
 #nave = pygame.transform.scale(pygame.image.load("images/rocket.gif"), (0, 0))
@@ -70,23 +73,34 @@ def main():
 
     while running:
         clock.tick(FPS)
-
         mouse_position = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                temp_obj_position = mouse_position
+                if temp_obj_position:
+                    t_x, t_y = temp_obj_position
+                    obj = aeronave(aeronave,t_x, t_y, 0, 0, pesoNave)
+                    objects.append(obj)
+                    temp_obj_position = None
+                else:
+                    temp_obj_position = mouse_position
 
         win.blit(background, (0, 0))
         if temp_obj_position:
             pygame.draw.line(win, branco, temp_obj_position, mouse_position, 1)
             pygame.draw.circle(win, vermelho, temp_obj_position, tam_nave)
 
+        for obj in objects:
+               obj.draw()
+
         pygame.display.update()
 
     pygame.quit()
+
+    #PAREI NO
 
 if __name__ == "__main__":
     main()
